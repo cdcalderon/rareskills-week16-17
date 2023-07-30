3e7d16d010af073d16faaa66603706145439a70d
carlos calderon <cdcalderon@gmail.com>
2023-07-30 15:55:38 -0500

    write verify contract

---
+++ scripts/verify-carlos-erc20.js
@@ -0,0 +1,21 @@
+const { run } = require("hardhat");
+
+const verify = async () => {
+  console.log("Verifying contract...");
+  try {
+    await run("verify:verify", {
+      address: "0x6Cd6013a8B6417D455fc524EE033f4B9b6fA43C0",
+      constructorArguments: [99],
+    });
+  } catch (e) {
+    if (e.message.toLowerCase().includes("already verified")) {
+      console.log("Already verified!");
+    } else {
+      console.log(e);
+    }
+  }
+};
+
+module.exports = {
+  verify,
+};
